47f3feb7b7aba17233fc196870293e9eb967da6a
snakeeyes <snakeeyes@snakeeyes-VirtualBox>
2018-01-15 15:04:42 -0500

    First Branch

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,6 +2,8 @@
 public class FirstClass {
 	public static void main(String[] args){
 		int a =10;
+		String s="Tweety";
 		System.out.println("Value of a is" + a);
+		System.out.println("\n" + s);
 	}
 }
